46f48d845b2bd31b100ff1b47ade88c01e920e39
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-06-22 09:43:22 -0500

    exercise 1 java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,49 @@
-public class HelloWorld {
+//public class HelloWorld {
+//
+//    //sout and tab will automatically fill in
+//    public static void main(String[] args) {
+//        System.out.println("Hello, world!");
+//    }
+//
+//}
 
-    //sout and tab will automatically fill in
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+public class HelloWorld{
+
+    public static void main(String[] args){
+//        int myFavoriteNumber = 2021;
+//        String myString = "It's a wonderful world ";
+//        myString += 3.1459;
+//
+//
+//        System.out.println(myFavoriteNumber);
+//        System.out.println(myString);
+
+//        float myNumber = (float)3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int)o;
+//        System.out.println(three);
+
+//        int x = 5;
+//        x *= 5;
+//        System.out.println(x);
+//
+//        int x = 20;
+//        x /= 5;
+//        System.out.println(x);
+
+
     }
-
 }
-
-
 
 
