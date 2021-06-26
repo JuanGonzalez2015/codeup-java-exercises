a75634e83614e39ad53c9e40e44bf1f910e5947e
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-06-26 09:59:22 -0500

    practice

---
+++ src/StringsLecture.java
@@ -0,0 +1,12 @@
+public class StringsLecture {
+
+ public static void main(String[] args){
+
+     String msg = "Hello";
+     System.out.println(msg);
+     msg = msg + ", how are you?";
+     System.out.println(msg);
+
+    }
+}
+
