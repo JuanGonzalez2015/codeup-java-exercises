ef5a37c22f67575977f23462a0307d232b51a13a
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-07-07 09:15:17 -0500

    warmup

---
+++ src/warmup/PrintUpperCaseLetterString.java
@@ -0,0 +1,12 @@
+package warmup;
+
+public class PrintUpperCaseLetterString{
+    public static void main(String[] args) {
+        String str = "We Are An American Band";
+        for(int i = 0; i < str.length(); i++) {
+            if(Character.isUpperCase(str.charAt(i))) {
+                System.out.println(str.charAt(i));
+            }
+        }
+    }
+}
