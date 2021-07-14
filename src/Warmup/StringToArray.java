d5b49e49cc65f945076cce434eec2c54fb220c7f
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-07-14 09:37:10 -0500

    warmup

---
+++ src/Warmup/StringToArray.java
@@ -0,0 +1,9 @@
+package Warmup;
+
+public class StringToArray {
+
+        public static String[] stringArray(String arg1){
+            return arg1.split(" ");
+        }
+
+    }
---
+++ src/Warmup/WarmUpMain.java
@@ -0,0 +1,9 @@
+package Warmup;
+
+import java.util.Arrays;
+
+public class WarmUpMain {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(StringToArray.stringArray("hey there buddy")));
+    }
+}
